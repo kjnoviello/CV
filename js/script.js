64edb6a630cdfbfbb9dbae9b4161e1315e95f924
kevin joel noviello <kjnoviello@gmail.com>
2023-02-27 12:38:25 -0300

    dark mode btn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.getElementById('btn').onclick = function () {
     alert('Su mensaje me fue enviado. Muchas gracias!');
 }
+
+document.getElementById('id-sun').onclick = function(){
+    document.getElementById('main_container').classList.remove('dark-mode')
+    document.getElementById('id-moon').classList.remove('active')
+    this.classList.add('active')
+}
+
+document.getElementById('id-moon').onclick = function(){
+    document.getElementById('main_container').classList.add('dark-mode')
+    document.getElementById('id-sun').classList.remove('active')
+    this.classList.add('active')
+}
